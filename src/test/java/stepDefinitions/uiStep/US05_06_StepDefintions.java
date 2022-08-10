6ee2faaeb08c36cbc71ca648c5abe1bbc9569a3c
murat akdeniz <akdenizmurat5205@gmail.com>
2022-08-10 23:59:32 +0300

    version99

---
+++ src/test/java/stepDefinitions/uiStep/US05_06_StepDefintions.java
@@ -0,0 +1,229 @@
+package stepDefinitions.uiStep;
+
+import com.github.javafaker.Faker;
+import io.cucumber.java.en.*;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.testng.Assert;
+import pages.US05_06_Page;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class US05_06_StepDefintions {
+
+    US05_06_Page us05_06_Page = new US05_06_Page();
+    Faker faker=new Faker();
+    String expectedCreatedDate;
+    int actualCreatedIndex;
+
+    @Given("Hasta {string} ana sayfasina gider")
+    public void hasta_ana_sayfasina_gider(String string) {
+        Driver.getDriver().get(ConfigReader.getProperty("medunnaUrl"));
+    }
+
+    @Given("Anasayfada giris ikonuna tiklar")
+    public void anasayfada_giris_ikonuna_tiklar() {
+        us05_06_Page.girisIkonu.click();
+    }
+
+    @Given("Sign in secenegine tiklar")
+    public void sign_in_secenegine_tiklar() {
+        us05_06_Page.signInTextElement.click();
+    }
+
+    @Given("Gecerli bir username girer")
+    public void gecerli_bir_username_girer() {
+        us05_06_Page.signInUsername.sendKeys(ConfigReader.getProperty("patientUser"));
+    }
+
+    @Given("Gecerli bir parola girer")
+    public void gecerli_bir_parola_girer() {
+        us05_06_Page.signInPassword.sendKeys(ConfigReader.getProperty("password"));
+    }
+
+    @Given("Sign in buttonuna tiklar")
+    public void sign_in_buttonuna_tiklar() {
+        us05_06_Page.signInButton.click();
+    }
+
+    @Given("My Pages\\(patient)e tiklar")
+    public void my_pages_patient_e_tiklar() {
+        us05_06_Page.myPagesPatient.click();
+    }
+
+    @Given("Make an Appointmenta tiklar")
+    public void make_an_appointmenta_tiklar() {
+        us05_06_Page.makeAnAppointmentElement.click();
+    }
+
+    @Then("First Name kutusunun bos birakilamayacagini kontrol eder")
+    public void first_name_kutusunun_bos_birakilamayacagini_kontrol_eder() throws InterruptedException {
+        us05_06_Page.makeAnAppointmentUserName.clear();
+        us05_06_Page.sendAnAppointmentRequest.click();
+
+        String expectedResult = "Your FirstName is required.";
+        String actualResult = us05_06_Page.firstNameRequiredTextElement.getText();
+
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+
+    @And("Sayfayi kapatir")
+    public void sayfayiKapatir() throws InterruptedException {
+        Thread.sleep(3000);
+        Driver.closeDriver();
+    }
+
+    @And("SSN kutusunun bos birakilamayacagini kontrol eder")
+    public void ssnKutusununBosBirakilamayacaginiKontrolEder() {
+        us05_06_Page.makeAnAppointmentSsn.clear();
+        us05_06_Page.sendAnAppointmentRequest.click();
+
+        String expectedResult = "Your SSN is required.";
+        String actualResult = us05_06_Page.ssnRequiredTextElement.getText();
+
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+
+    @And("Last Name kutusunun bos birakilamayacagini kontrol eder")
+    public void lastNameKutusununBosBirakilamayacaginiKontrolEder() {
+        us05_06_Page.makeAnAppointmentLastName.clear();
+        us05_06_Page.sendAnAppointmentRequest.click();
+
+        String expectedResult = "Your LastName is required.";
+        String actualResult = us05_06_Page.lastNameRequiredTextElement.getText();
+
+        Assert.assertEquals(expectedResult, actualResult);
+
+    }
+
+    @And("Phone kutusunun bos birakilamayacagini kontrol eder")
+    public void phoneKutusununBosBirakilamayacaginiKontrolEder() {
+        us05_06_Page.makeAnAppointmentPhone.clear();
+        us05_06_Page.sendAnAppointmentRequest.click();
+
+        String expectedResult = "Phone number is required.";
+        String actualResult = us05_06_Page.phoneNumberRequiredTextElement.getText();
+
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+
+    @Given("Randevu formuna telefon numarasi girer")
+    public void randevu_formuna_telefon_numarasi_girer() {
+        us05_06_Page.makeAnAppointmentPhone.sendKeys(ConfigReader.getProperty("patientPhoneNumber"));
+    }
+
+    @Given("Send an Appointment Requeste tiklar")
+    public void send_an_appointment_requeste_tiklar() {
+        us05_06_Page.sendAnAppointmentRequest.click();
+        LocalDateTime myDateObj = LocalDateTime.now();
+        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd/MM/yy HH:mm");
+        expectedCreatedDate = myDateObj.format(myFormatObj);
+    }
+
+    @Given("Randevu olusturuldugunu kontrol eder")
+    public void randevu_olusturuldugunu_kontrol_eder() {
+        //Assert.assertTrue(us05_06_Page.appointmentSavedTextElement.isDisplayed());
+
+    }
+
+    @Given("My Appointmentsa tiklar")
+    public void my_appointmentsa_tiklar() {
+        us05_06_Page.makeAppointmentsElement.click();
+    }
+
+    @Given("Ramdevularin goruntulendigini kontrol eder")
+    public void ramdevularin_goruntulendigini_kontrol_eder() {
+
+
+        for (int i = 0; i < us05_06_Page.createdDateElements.size() ; i++) {
+            JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+            js.executeScript("arguments[0].scrollIntoView();", us05_06_Page.createdDateElements.get(i));
+            if(us05_06_Page.createdDateElements.get(i).getText().contains(expectedCreatedDate)){
+                actualCreatedIndex=i;
+                break;
+            }
+        }
+        String actualCreatedDate=us05_06_Page.createdDateElements.get(actualCreatedIndex).getText();
+
+        Assert.assertEquals(actualCreatedDate, expectedCreatedDate);
+        System.out.println();
+    }
+
+    @Given("Settingse tiklar")
+    public void settingse_tiklar() {
+        us05_06_Page.settings.click();
+    }
+
+    @Given("First Name bilgisinin dogrulugunu kontrol eder")
+    public void first_name_bilgisinin_dogrulugunu_kontrol_eder() throws InterruptedException {
+        String expectedFirstName = ConfigReader.getProperty("userFirstName");
+        String actualFirstName = us05_06_Page.settingsFirstName.getText();
+
+        Assert.assertEquals(actualFirstName, expectedFirstName);
+    }
+
+    @Given("Last Name bilgisinin dogrulugunu kontrol eder")
+    public void last_name_bilgisinin_dogrulugunu_kontrol_eder() {
+        String expectedLastName = ConfigReader.getProperty("userLastName");
+        String actualLastName = us05_06_Page.settingsLastName.getText();
+
+        Assert.assertEquals(actualLastName, expectedLastName);
+
+    }
+
+    @Given("Email bilgisinin dogrulugunu kontrol eder")
+    public void email_bilgisinin_dogrulugunu_kontrol_eder() {
+        String expectedEmail = ConfigReader.getProperty("userEmail");
+        String actualEmail = us05_06_Page.settingsEmail.getText();
+
+        //Assert.assertEquals(actualEmail, expectedEmail);
+
+    }
+
+    @And("First Name bilgisinin degistirilebildigini kontrol eder")
+    public void firstNameBilgisininDegistirilebildiginiKontrolEder() {
+        us05_06_Page.settingsFirstName.clear();
+        us05_06_Page.settingsFirstName.sendKeys(ConfigReader.getProperty("newUserFirstName") + Keys.ENTER);
+        Driver.getDriver().navigate().refresh();
+
+        String expectedFirstName = ConfigReader.getProperty("newUserFirstName");
+        String actualFirstName = us05_06_Page.settingsFirstName.getText();
+
+        Assert.assertEquals(actualFirstName, expectedFirstName);
+
+        us05_06_Page.settingsFirstName.clear();
+        us05_06_Page.settingsFirstName.sendKeys(ConfigReader.getProperty("userFirstName") + Keys.ENTER);
+
+    }
+
+    @And("Last Name bilgisinin degistirilebildigini kontrol eder")
+    public void lastNameBilgisininDegistirilebildiginiKontrolEder() {
+        us05_06_Page.settingsLastName.clear();
+        us05_06_Page.settingsLastName.sendKeys(ConfigReader.getProperty("newUserLastName") + Keys.ENTER);
+        Driver.getDriver().navigate().refresh();
+
+        String expectedLastName = ConfigReader.getProperty("newUserLastName");
+        String actualLastName = us05_06_Page.settingsFirstName.getText();
+
+        Assert.assertEquals(actualLastName, expectedLastName);
+
+        us05_06_Page.settingsLastName.clear();
+        us05_06_Page.settingsLastName.sendKeys(ConfigReader.getProperty("userLastName") + Keys.ENTER);
+
+    }
+
+    @And("Email bilgisinin degistirilebildigini kontrol eder")
+    public void emailBilgisininDegistirilebildiginiKontrolEder() {
+        us05_06_Page.settingsEmail.clear();
+        String firstEmail = us05_06_Page.settingsEmail.getText();
+        us05_06_Page.settingsEmail.sendKeys(faker.internet().emailAddress() + Keys.ENTER);
+        Driver.getDriver().navigate().refresh();
+
+        String actualEmail = us05_06_Page.settingsEmail.getText();
+
+        Assert.assertEquals(actualEmail, firstEmail);
+    }
+}
---
+++ src/test/java/stepDefinitions/uiStep/US11_Stepdefinitions.java
@@ -0,0 +1,100 @@
+package stepDefinitions.uiStep;
+
+import com.github.javafaker.Faker;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import pages.US11_Pages;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class US11_Stepdefinitions {
+    US11_Pages us11_pages=new US11_Pages();
+
+    Actions actions = new Actions(Driver.getDriver());
+    Faker faker = new Faker();
+    String fakerName = faker.name().fullName();
+
+    @Given("kullanici {string} anasayfasindaa")
+    public void kullanici_anasayfasinda(String medunna) {
+        Driver.getDriver().get(ConfigReader.getProperty("medunnaUrl"));
+    }
+    @Then("doktor signInbutonu tiklar")
+    public void doktor_sign_ınbutonu_tiklar() {
+        us11_pages.signInButonu.click();
+    }
+    @Then("doktor loginButonu tiklar")
+    public void doktor_login_butonu_tiklar() {
+        us11_pages.loginButonu.click();
+    }
+    @Then("doktor {string} ve {string} girer SignIn tıklar")
+    public void doktor_ve_girer_sign_ın_tıklar(String string, String string2) {
+        us11_pages.username.sendKeys("doctor-healthprojectteam56" + Keys.TAB);
+        us11_pages.password.sendKeys("Teamproject.56" + Keys.TAB);
+
+
+        actions
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.TAB)
+                .perform();
+        us11_pages.signIn.click();
+    }
+    @Then("doktor mypages butonuna tiklar")
+    public void doktor_mypages_butonuna_tiklar() {
+        us11_pages.myPages.click();
+    }
+    @Then("doktor myAppointments butonuna tiklar")
+    public void doktor_my_appointments_butonuna_tiklar() {
+        us11_pages.myAppointments.click();
+    }
+    @Then("doktor edit butonuna tiklar")
+    public void doktor_edit_butonuna_tiklar() {
+        us11_pages.editButonu.click();
+    }
+    @Then("doktor Create or Edit an Appointment yazisini gorur")
+    public void doktor_create_or_edit_an_appointment_yazisini_gorur() {
+        Assert.assertTrue(us11_pages.createYazisi.isDisplayed());
+
+    }
+
+    @Given("doktor {string},{string},{string},{string},{string} gorebilmeli")
+    public void doktorGorebilmeli(String ID, String SrartDateTime, String EndDataTime, String Status, String aPhysician) {
+        Assert.assertTrue(us11_pages.Id.isDisplayed());
+        Assert.assertTrue(us11_pages.startDateTime.isDisplayed());
+        Assert.assertTrue(us11_pages.endDateTime.isDisplayed());
+        Assert.assertTrue(us11_pages.status.isDisplayed());
+        Assert.assertTrue(us11_pages.patient.isDisplayed());
+    }
+
+    @Given("doktor {string},{string} ve {string} alanlarini doldurmali")
+    public void doktorVeAlanlariniDoldurmali(String Anamnesis, String Treatment, String Diagnosis) {
+        actions.click(us11_pages.anamnesis)
+                .sendKeys(fakerName)
+                .sendKeys(Keys.TAB)
+                .sendKeys(fakerName)
+                .sendKeys(Keys.TAB)
+                .sendKeys(fakerName)
+                .perform();
+    }
+
+
+    @Given("doktor {string},{string} doldurmali")
+    public void doktorDoldurmali(String prescription, String description) {
+        us11_pages.prescription.sendKeys(fakerName);
+        us11_pages.description.sendKeys(fakerName);
+    }
+
+    @Given("doktor status olarak secebilmeli")
+    public void doktorOlarakSecebilmeli(String status) {
+        WebElement ddo= us11_pages.status;
+        Select select= new Select(ddo);
+        select.selectByVisibleText("PENDING");
+        select.selectByVisibleText("COMPLETED");
+        select.selectByVisibleText("CANCELLED");
+    }
+}
