01bcd315edb3f252d49717a7c61bff6d4de8005e
hamza <hersoy11@gmail.com>
2022-08-10 12:49:01 +0300

    hamza version-1

---
+++ src/test/java/pages/US14_Pages.java
@@ -0,0 +1,75 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class US14_Pages {
+
+    public US14_Pages (){
+        PageFactory.initElements(Driver.getDriver(),this);
+
+
+    }
+    @FindBy(xpath = "//*[@class='d-flex align-items-center dropdown-toggle nav-link']")
+    public WebElement girisButon;
+
+    @FindBy(xpath = "(//*[@class='dropdown-item'])[1]")
+    public WebElement singInButon;
+
+    @FindBy(xpath = "//input[@name='username']")
+    public WebElement kullaniciAdi;
+
+    @FindBy(xpath = "//input[@name='password']")
+    public WebElement password;
+
+    @FindBy(xpath = "//button[@class='btn btn-primary']")
+    public WebElement singInGiris;
+
+    @FindBy(xpath = "//*[text()='MY PAGES']")
+    public WebElement myPagesButon;
+
+    @FindBy(xpath = "(//*[@class='dropdown-item'])[3]")
+    public WebElement myInpatientsButon;
+
+    @FindBy(xpath = "//th[1]")
+    public WebElement id;
+
+    @FindBy(xpath = "//th[2]")
+    public WebElement startDate;
+
+    @FindBy(xpath = "//th[3]")
+    public WebElement endDate;
+
+    @FindBy(xpath = "//th[4]")
+    public WebElement status;
+
+    @FindBy(xpath = "//th[5]")
+    public WebElement description;
+
+    @FindBy(xpath = "//th[6]")
+    public WebElement createdDate;
+
+    @FindBy(xpath = "//th[7]")
+    public WebElement room;
+
+    @FindBy(xpath = "//th[8]")
+    public WebElement appointment;
+
+    @FindBy(xpath = "//th[9]")
+    public WebElement patient;
+
+    @FindBy (xpath = "(//*[@class='btn btn-primary btn-sm'])[2]")
+    public WebElement edit;
+
+    @FindBy (xpath = "//*[@id='in-patient-status']")
+    public WebElement statusDd;
+
+    @FindBy(xpath = "//button[@id='save-entity']")
+    public WebElement save;
+
+    @FindBy(xpath = "//select[@id='in-patient-room']")
+    public WebElement roomDd;
+
+}
---
+++ src/test/java/stepDefinitions/uiStep/US14_StepDefinitions.java
@@ -0,0 +1,151 @@
+package stepDefinitions.uiStep;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import pages.US14_Pages;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class US14_StepDefinitions {
+
+US14_Pages us14 = new US14_Pages();
+
+    @Given("Doktor Medunna ana sayfasina gider")
+    public void doktor_medunna_ana_sayfasina_gider() {
+        Driver.getDriver().get(ConfigReader.getProperty("medunnaUrl"));
+
+    }
+    @When("Anasayfada giris butonuna tiklar")
+    public void anasayfada_giris_butonuna_tiklar() {
+        us14.girisButon.click();
+
+    }
+
+    @Then("Gecerli bir kullaniciAdi girer")
+    public void gecerli_bir_username_girer() {
+        us14.kullaniciAdi.sendKeys(ConfigReader.getProperty("doctorUser"));
+
+    }
+    @Then("Gecerli bir sifre girer")
+    public void gecerli_bir_parola_girer() {
+        us14.password.sendKeys(ConfigReader.getProperty("password"));
+
+    }
+    @Then("SignIn buttonuna tiklar")
+    public void sign_in_buttonuna_tiklar() {
+        us14.singInGiris.click();
+
+    }
+    @Then("kullanici {int} saniye bekler")
+    public void kullanici_saniye_bekler(int istenenSaniye) {
+
+        try {
+            Thread.sleep(istenenSaniye*1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Then("MY PAGES menusune tiklar")
+    public void my_pages_menusune_tiklar() {
+        us14.myPagesButon.click();
+
+    }
+    @Then("MY PAGES menuden My Inpatients'i secer")
+    public void my_pages_menuden_my_ınpatients_i_secer() {
+        us14.myInpatientsButon.click();
+
+    }
+    @Then("Cikan sayfada \"\"ID, Start Date, End date, Satatus, Description, Created Date, Room, Appointment id and Patient\" bilgilerinin goruntulenebildigini test eder")
+    public void cikan_sayfada_ıd_start_date_end_date_satatus_description_created_date_room_appointment_id_and_patient_bilgilerinin_goruntulenebildigini_test_eder() {
+
+        Assert.assertTrue(us14.id.isDisplayed());
+        Assert.assertTrue(us14.startDate.isDisplayed());
+        Assert.assertTrue(us14.endDate.isDisplayed());
+        Assert.assertTrue(us14.status.isDisplayed());
+        Assert.assertTrue(us14.description.isDisplayed());
+        Assert.assertTrue(us14.createdDate.isDisplayed());
+        Assert.assertTrue(us14.room.isDisplayed());
+        Assert.assertTrue(us14.appointment.isDisplayed());
+        Assert.assertTrue(us14.patient.isDisplayed());
+
+    }
+
+    @Then("kullanici sayfayi kapatir")
+    public void kullanici_sayfayi_kapatir() {
+        Driver.closeDriver();
+    }
+
+    @Then("Cikan sayfada edit butonuna tiklanir")
+    public void cikan_sayfada_edit_butonuna_tiklanir() {
+        us14.edit.click();
+        // WebDriver driver = new ChromeDriver();
+        // JavascriptExecutor js = (JavascriptExecutor)driver;
+        // js.executeScript("window.scrollBy(0, 250)", "");
+    }
+    @Then("status menusunden {string} sekmesi secilerek save tusuna basilir. \"UNAPPROVED\"yazisi goruntulenir")
+    public void status_menusunden_sekmesi_secilerek_save_tusuna_basilir_unapproved_yazisi_goruntulenir(String string) throws InterruptedException {
+        Actions action = new Actions(Driver.getDriver());
+        action.sendKeys(Keys.PAGE_DOWN).build().perform();
+
+        Select select = new Select(us14.statusDd);
+        select.selectByVisibleText("UNAPPROVED");
+        Thread.sleep(2000);
+        us14.save.submit();
+
+
+    }
+    @Then("status menusunden {string} sekmesi secilerek save tusuna basilir. \"STAYING\"yazisi goruntulenir")
+    public void status_menusunden_sekmesi_secilerek_save_tusuna_basilir_stayıng_yazisi_goruntulenir(String string) throws InterruptedException {
+        Thread.sleep(8000);
+        Select select = new Select(us14.statusDd);
+        select.selectByVisibleText("STAYING");
+        Thread.sleep(2000);
+        us14.save.submit();
+    }
+    @Then("status menusunden {string} sekmesi secilerek save tusuna basilir. \"DISCHARGED\"yazisi goruntulenir")
+    public void status_menusunden_sekmesi_secilerek_save_tusuna_basilir_dıscharged_yazisi_goruntulenir(String string) throws InterruptedException {
+        Thread.sleep(8000);
+        Select select = new Select(us14.statusDd);
+        select.selectByVisibleText("DISCHARGED");
+        Thread.sleep(2000);
+        us14.save.submit();
+    }
+    @Then("status menusunden {string} sekmesi secilerek save tusuna basilir. \"CANCELLED\"yazisi goruntulenir")
+    public void status_menusunden_sekmesi_secilerek_save_tusuna_basilir_cancelled_yazisi_goruntulenir(String string) throws InterruptedException {
+        Thread.sleep(8000);
+        Select select = new Select(us14.statusDd);
+        select.selectByVisibleText("CANCELLED");
+        Thread.sleep(2000);
+        us14.save.submit();
+    }
+
+    @Then("Cikan sayfada yatan bir hastanin edit butonuna tiklanir")
+    public void cikan_sayfada_yatan_bir_hastanin_edit_butonuna_tiklanir() {
+        us14.edit.click();
+
+    }
+    @Then("{string} menusunden herhangi bir oda secilerek save tusuna basilir.")
+    public void menusunden_herhangi_bir_oda_secilerek_save_tusuna_basilir(String string) throws InterruptedException {
+        Actions action = new Actions(Driver.getDriver());
+        action.sendKeys(Keys.PAGE_DOWN).build().perform();
+
+        Select select = new Select(us14.roomDd);
+        select.selectByIndex(7);
+        Thread.sleep(2000);
+
+        us14.save.click();
+
+    }
+    @Then("Yeni sayfada degisikligin oldugu goruntulenir.")
+    public void yeni_sayfada_degisikligin_oldugu_goruntulenir() {
+
+
+    }
+}
